9e0ac628afdc79b6d7a97bef770146c3a5039b07
batangaza <batangaza@gmail.com>
2019-11-06 09:13:13 +0300

    smaol project

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 //require is a key word that helps us get the packages we finna use
 //we want to create a server where browsers can connect
-app.listen(202, function(){
-console.log('listening on 1000')
+app.listen(3000, ()=>{
+console.log('listening on 3000')
 })
 /*the browser reads by getting. app=express as earlier declared
 app.get(path, callback)*/
-app.get('/', function(req,res){
+app.get('/',(req,res)=>{
     res.send('Hello World')
 }
 )
+app.get('/about',(req,res)=>{
+    res.send('This is an about us page')
+}
+)
+
